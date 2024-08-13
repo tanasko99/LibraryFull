355badf0f484c057dd3d5ddfe39f5362fa5deb7e
tanasko99 <tanaskovicvuk99@gmail.com>
2024-08-13 14:22:17 +0200

    crud author

--- Services/IAuthorService.cs
+++ Services/IAuthorService.cs
@@ -1,9 +1,14 @@
-﻿using FullLibrary.DTOs;
+﻿using FluentResults;
+using FullLibrary.DTOs;
 
 namespace FullLibrary.Services
 {
     public interface IAuthorService
     {
         void CreateAuthor(AuthorDto author);
+        void UpdateAuthor(int id,AuthorUpdateDto author);
+        IList<AuthorDto> GetAllAuthors();
+        Result<AuthorDto> GetAuthorById(int id);
+        bool DeleteAuthor(int id);
     }
 }
